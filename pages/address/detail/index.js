afc6de8d47da200df0cf48500fb0d30f691a7d8d
hikelee <hikelee@gmail.com>
2018-08-18 08:13:32 +0800

    update address

--- pages/address/detail/index.js
+++ pages/address/detail/index.js
@@ -1,5 +1,5 @@
-var commonCityData = require('../../../utils/city.js')
-//获取应用实例
+var commonCityData = require('../../../utils/city.js');
+var util = require('../../../utils/util.js')
 var app = getApp()
 Page({
     data: {
@@ -17,43 +17,25 @@ Page({
         wx.navigateBack({})
     },
     bindSave: function (e) {
+        console.log(e.detail)
         var that = this;
         var linkMan = e.detail.value.linkMan;
         var address = e.detail.value.address;
         var mobile = e.detail.value.mobile;
         var code = e.detail.value.code;
+        var isDefault = e.detail.value.isDefault;
 
         if (linkMan == "") {
-            wx.showModal({
-                title: '提示',
-                content: '请填写联系人姓名',
-                showCancel: false
-            })
-            return
+            return util.alert('提示','请填写联系人姓名');
         }
         if (mobile == "") {
-            wx.showModal({
-                title: '提示',
-                content: '请填写手机号码',
-                showCancel: false
-            })
-            return
+            return util.alert('提示','请填写手机号码');
         }
         if (this.data.selProvince == "请选择") {
-            wx.showModal({
-                title: '提示',
-                content: '请选择地区',
-                showCancel: false
-            })
-            return
+            return util.alert('提示','请选择地区');
         }
         if (this.data.selCity == "请选择") {
-            wx.showModal({
-                title: '提示',
-                content: '请选择地区',
-                showCancel: false
-            })
-            return
+            return util.alert('提示','请选择地区');
         }
         var cityId = commonCityData.cityData[this.data.selProvinceIndex].cityList[this.data.selCityIndex].id;
         var districtId;
@@ -63,20 +45,10 @@ Page({
             districtId = commonCityData.cityData[this.data.selProvinceIndex].cityList[this.data.selCityIndex].districtList[this.data.selDistrictIndex].id;
         }
         if (address == "") {
-            wx.showModal({
-                title: '提示',
-                content: '请填写详细地址',
-                showCancel: false
-            })
-            return
+            return util.alert('提示','请填写详细地址');
         }
         if (code == "") {
-            wx.showModal({
-                title: '提示',
-                content: '请填写邮编',
-                showCancel: false
-            })
-            return
+            return util.alert('提示','请填写邮编');
         }
         var apiAddoRuPDATE = "add";
         var apiAddid = that.data.id;
@@ -97,7 +69,7 @@ Page({
                 address: address,
                 mobile: mobile,
                 code: code,
-                isDefault: 'true'
+                isDefault: isDefault
             },
             success: function (res) {
                 if (res.data.code != 0) {
@@ -181,14 +153,10 @@ Page({
         this.initCityData(1);
         var id = e.id;
         if (id) {
-            // 初始化原数据
             wx.showLoading();
             wx.request({
                 url: app.globalData.urlPrefix + '/address/detail',
-                data: {
-                    token: wx.getStorageSync('token'),
-                    id: id
-                },
+                data: {  token: wx.getStorageSync('token'), id: id  },
                 success: function (res) {
                     wx.hideLoading();
                     if (res.data.code == 0) {
@@ -230,9 +198,7 @@ Page({
             }
         }
     },
-    selectCity: function () {
-
-    },
+    selectCity: function () {  },
     deleteAddress: function (e) {
         var that = this;
         var id = e.currentTarget.dataset.id;
